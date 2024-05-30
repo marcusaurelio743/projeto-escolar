dbfaae355642f2cfffb105c56ee11121daf8d6e3
marcus aurelio <marcusaurelio743@gmail.com>
2024-05-30 18:47:14 -0300

    criando turma

--- escola/src/main/java/escola/model/entity/Aluno.java
+++ escola/src/main/java/escola/model/entity/Aluno.java
@@ -1,12 +1,14 @@
 package escola.model.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -38,6 +40,9 @@ public class Aluno {
 	
 	@Column(nullable = false, length = 100)
 	private String sexo;
+	
+	@OneToMany
+	private List<Turma> turmas;
 	
 	private LocalDate dataCadastro;
 	
